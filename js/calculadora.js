bc6a4b4a9b5d500ee5bcd29cdc7e30093be2575f
Cauã Mendonça <cauamendooncaa55@gmail.com>
2024-04-12 23:45:55 -0300

    cor atl

---
+++ js/calculadora.js
@@ -0,0 +1,53 @@
+ function Equipes(){
+
+   var selectEquipes = document.getElementById("cor")
+
+   
+
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ function calculadora(){
+
+    let cor = document.getElementById("select")[0].value
+    let mensagem
+
+    if(cor == "Amarela"){
+
+
+    }else if(cor == "Laranja"){
+
+    }else if(cor == "Roxa"){
+
+    }else if(cor == "Verde"){
+
+    }else if(cor == "Vermelho"){
+
+    }else{
+
+    }
+
+
+
+
+
+
+
+
+ }
